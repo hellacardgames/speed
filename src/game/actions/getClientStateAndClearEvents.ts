c9d5866ab8abf421b3091b323dea706641907712
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-25 10:38:40 -0700

    Update lib and use getClientStateAndClearEventsFactory

--- src/game/actions/getClientStateAndClearEvents.ts
+++ src/game/actions/getClientStateAndClearEvents.ts
@@ -1,30 +1,26 @@
+import { getClientStateAndClearEventsFactory } from "@hellacardgames/lib";
 import type { ClientState } from "../types/ClientState.js";
 import type { Game } from "../types/Game.js";
 
-export function getClientStateAndClearEvents(game: Game, playerId: string) {
-  const player = game.players.find((p) => p.id === playerId);
-  if (!player) {
-    return { success: false, error: "playerNotFound" } as const;
-  }
-  const state: ClientState = {
-    status: game.status,
-    gameId: game.id,
-    playerId,
-    username: player.username,
-    players: game.players.map((p) => ({
-      username: p.username,
-      handSize: p.hand.length,
-      drawPileSize: p.drawPile.length,
-      sidePileSize: p.sidePile.length,
-      centerPileTopCard: p.centerPile.length
-        ? p.centerPile[p.centerPile.length - 1]!
-        : null,
-    })),
-    expiresAt: game.expiresAt,
-    chatMessages: game.chatMessages,
-    hand: player.hand,
-    canPlayAt: game.status === "started" ? game.canPlayAt : null,
-  };
-  player.events.length = 0;
-  return { success: true, state, game } as const;
-}
+export const getClientStateAndClearEvents = getClientStateAndClearEventsFactory<
+  Game,
+  ClientState
+>((game, player) => ({
+  status: game.status,
+  gameId: game.id,
+  playerId: player.id,
+  username: player.username,
+  players: game.players.map((p) => ({
+    username: p.username,
+    handSize: p.hand.length,
+    drawPileSize: p.drawPile.length,
+    sidePileSize: p.sidePile.length,
+    centerPileTopCard: p.centerPile.length
+      ? p.centerPile[p.centerPile.length - 1]!
+      : null,
+  })),
+  expiresAt: game.expiresAt,
+  chatMessages: game.chatMessages,
+  hand: player.hand,
+  canPlayAt: game.status === "started" ? game.canPlayAt : null,
+}));
--- src/game/index.ts
+++ src/game/index.ts
@@ -3,13 +3,13 @@ export { MAX_PLAYERS } from "./constants.js";
 export { createGame } from "./actions/createGame.js";
 export { drawCard } from "./actions/drawCard.js";
 export { getClientStateAndClearEvents } from "./actions/getClientStateAndClearEvents.js";
+export { getEventsAndClearAcknowledged } from "./actions/getEventsAndClearAcknowledged.js";
 export { joinGame } from "./actions/joinGame.js";
 export { leaveGame } from "./actions/leaveGame.js";
 export { playCard } from "./actions/playCard.js";
 export { reportNoPlayableCards } from "./actions/reportNoPlayableCards.js";
+export { sendChat } from "./actions/sendChat.js";
 export { startGame } from "./actions/startGame.js";
-
-export { getEventsAndClearAcknowledged, sendChat } from "@hellacardgames/lib";
 
 export type { Card } from "./types/Card.js";
 export type { ChatMessage } from "./types/ChatMessage.js";
