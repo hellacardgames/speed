1ff76317b14e720bfe4ef22752dd897d44edf7e8
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-25 14:49:39 -0700

    Update lib and createManager

--- src/manager/createManager.ts
+++ src/manager/createManager.ts
@@ -22,47 +22,9 @@ export const createManager = createManagerFactory({
   leaveGame,
   sendChat,
   startGame,
-  createCustomActions: (games) => ({
-    drawCard: (gameId: string, playerId: string) => {
-      const game = games.get(gameId);
-      if (!game) {
-        return { success: false, error: "gameNotFound" } as const;
-      }
-      const result = drawCard(game, playerId);
-      if (!result.success) {
-        return { success: false, error: result.error } as const;
-      }
-      games.set(gameId, result.game);
-      return { success: true } as const;
-    },
-    playCard: (
-      gameId: string,
-      playerId: string,
-      cardId: string,
-      isForOtherPlayerPile: boolean,
-    ) => {
-      const game = games.get(gameId);
-      if (!game) {
-        return { success: false, error: "gameNotFound" } as const;
-      }
-      const result = playCard(game, playerId, cardId, isForOtherPlayerPile);
-      if (!result.success) {
-        return { success: false, error: result.error } as const;
-      }
-      games.set(gameId, result.game);
-      return { success: true } as const;
-    },
-    reportNoPlayableCards: (gameId: string, playerId: string) => {
-      const game = games.get(gameId);
-      if (!game) {
-        return { success: false, error: "gameNotFound" } as const;
-      }
-      const result = reportNoPlayableCards(game, playerId);
-      if (!result.success) {
-        return { success: false, error: result.error } as const;
-      }
-      games.set(gameId, result.game);
-      return { success: true } as const;
-    },
+  addCustomActions: (wrapAction) => ({
+    drawCard: wrapAction(drawCard),
+    playCard: wrapAction(playCard),
+    reportNoPlayableCards: wrapAction(reportNoPlayableCards),
   }),
 });
