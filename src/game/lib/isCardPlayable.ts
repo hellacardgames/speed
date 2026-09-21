92e1e637f3f5d18768e67e0915fd62cc17a6c6e2
Richard Gieg <richgieg@users.noreply.github.com>
2026-09-20 17:35:58 -0700

    Update lib

--- src/game/lib/isCardPlayable.ts
+++ src/game/lib/isCardPlayable.ts
@@ -1,10 +1,10 @@
-import { getRankValue } from "@hellacardgames/lib";
+import { getAceHighRankValue } from "@hellacardgames/lib";
 import type { Card } from "../types/Card.js";
 
 export function isCardPlayable(card: Card, targetPile: readonly Card[]) {
   const targetCard = targetPile[targetPile.length - 1]!;
-  const rankValue = getRankValue(card.rank);
-  const targetRankValue = getRankValue(targetCard.rank);
+  const rankValue = getAceHighRankValue(card.rank);
+  const targetRankValue = getAceHighRankValue(targetCard.rank);
   const diff = Math.abs(rankValue - targetRankValue) % 11;
   return diff === 1;
 }
